98d1518cf29257b3ea1a72de2e0376e57c461bdf
dymafr <erwan1309@hotmail.com>
2022-03-01 12:01:28 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <Product />
+  <Product />
+  <Product />
+</template>
+
+<script setup lang="ts">
+import Product from './Product.vue';
+</script>
+
+<style scoped lang="scss"></style>
